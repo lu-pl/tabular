bb318e3b06c226773cec63de64ce7b1c17410c61
Lukas Plank <lupl@tuta.io>
2023-07-20 10:45:25 +0200

    Added RenderStrategy type

--- tabular/tabular_types.py
+++ tabular/tabular_types.py
@@ -1,11 +1,14 @@
 """Type definition for tabuler"""
 
 from collections.abc import Callable, MutableMapping
-from typing import Any
+from typing import Any, Literal as PyLiteral
 
 from rdflib import Graph, URIRef, Literal
 
 _Rule = Callable[[Any, Any, MutableMapping], Graph]
 _RulesMapping = MutableMapping[str, _Rule]
+
 _TripleObject = URIRef | Literal
 _Triple = tuple[URIRef, URIRef, _TripleObject]
+
+_RenderStrategy = PyLiteral["table", "row"]
