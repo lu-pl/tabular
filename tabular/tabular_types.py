0e2464234a04acf36dae2e86b87e9d37e54ba10d
Lukas Plank <lupl@tuta.io>
2023-07-25 10:17:20 +0200

    Added TemplateReference type

--- tabular/tabular_types.py
+++ tabular/tabular_types.py
@@ -1,8 +1,11 @@
 """Type definition for tabuler"""
 
+import os
+
 from collections.abc import Callable, MutableMapping
 from typing import Any, Literal as PyLiteral
 
+from jinja2.environment import Template
 from rdflib import Graph, URIRef, Literal
 
 _Rule = Callable[[Any, Any, MutableMapping], Graph]
@@ -12,3 +15,5 @@ _TripleObject = URIRef | Literal
 _Triple = tuple[URIRef, URIRef, _TripleObject]
 
 _RenderStrategy = PyLiteral["table", "row"]
+
+_TemplateReference = str | os.PathLike | Template
