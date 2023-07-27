51e5ecb6719e3d264382752d580daae6d7063e7f
Lukas Plank <lupl@tuta.io>
2023-07-27 15:32:14 +0200

    Implented CLI interface

--- tabular/tabular-cli.py
+++ tabular/tabular-cli.py
@@ -19,7 +19,12 @@ _ClickPath = click.Path(
 )
 
 
-@click.command()
+@click.group()
+def tabular_cli():
+    pass
+
+
+@tabular_cli.command()
 @click.argument("table_file",
                 type=_ClickPath)
 @click.argument("template_file",
@@ -32,15 +37,59 @@ _ClickPath = click.Path(
               type=tuple,
               cls=RequiredMultiOptions,
               required_if="column")
-def tabular_cli(table_file: pathlib.Path,
-                template_file: pathlib.Path,
-                column: str,
-                rows: tuple[Any]):
-    """.."""
+@click.option("--render-by-row",
+              type=bool,
+              default=False)
+def text(table_file: pathlib.Path,
+         template_file: pathlib.Path,
+         column: str,
+         rows: tuple[Any, ...],
+         render_by_row):
+    """..."""
+    # 1. get a dataframe
+    # optional: partition a dataframe
+
+    # 2. get a Converter
+
+    # 3. render according to strategy (table or row)
+
     print(table_file)
     print(template_file)
     print(type(table_file))
     print(type(template_file))
+    print()
+    print(column)
+    print(rows)
+
+
+@tabular_cli.command()
+@click.argument("table_file",
+                type=_ClickPath)
+@click.argument("template_file",
+                type=_ClickPath)
+@click.option("-c", "--column",
+              type=str,
+              cls=RequiredIf,
+              required_if="rows")
+@click.option("-r", "--rows",
+              type=tuple,
+              cls=RequiredMultiOptions,
+              required_if="column")
+@click.option("-f", "--format",
+              type=str,
+              default="ttl")
+def graph(table_file: pathlib.Path,
+          template_file: pathlib.Path,
+          column: str,
+          rows: tuple[Any, ...],
+          format):
+    """..."""
+    print(table_file)
+    print(template_file)
+    print()
+    print(column)
+    print(rows)
+    print(format)
 
 
 if __name__ == "__main__":
