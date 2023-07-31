f505f65dfe5ba50fd6ef25457ad676719994b972
Lukas Plank <lupl@tuta.io>
2023-07-31 12:52:08 +0200

    Added graph serializer Literal type

--- tabular/tabular_types.py
+++ tabular/tabular_types.py
@@ -1,12 +1,19 @@
 """Type definition for tabuler"""
 
 import os
+import pathlib
 
 from collections.abc import Callable, MutableMapping
 from typing import Any, Literal as PyLiteral
 
+import click
+
 from jinja2.environment import Template
 from rdflib import Graph, URIRef, Literal
+
+from rdflib.plugin import plugins
+from rdflib.serializer import Serializer
+
 
 _Rule = Callable[[Any, Any, MutableMapping], Graph]
 _RulesMapping = MutableMapping[str, _Rule]
@@ -17,3 +24,21 @@ _Triple = tuple[URIRef, URIRef, _TripleObject]
 _RenderStrategy = PyLiteral["table", "row"]
 
 _TemplateReference = str | os.PathLike | Template
+
+_ClickPath = click.Path(
+    exists=True,
+    file_okay=True,
+    dir_okay=False,
+    readable=True,
+    resolve_path=True,
+    path_type=pathlib.Path
+)
+
+rdflib_graph_format_options: str = [
+    plugin.name for plugin
+    in plugins()
+    if plugin.kind == Serializer
+    ]
+
+_GraphFormatOptions = PyLiteral[*rdflib_graph_format_options]
+_GraphFormatOptionsChoice = click.Choice(rdflib_graph_format_options)
