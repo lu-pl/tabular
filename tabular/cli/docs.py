aa92df3f6a4a9df94deeba22224a8b7432274af7
Lukas Plank <lupl@tuta.io>
2023-08-01 16:00:53 +0200

    Minor formatting

--- tabular/cli/docs.py
+++ tabular/cli/docs.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 @dataclass
 class CLIDocs:
     """Data container for TabulaR CLI documentation."""
+
     column: str
     rows: str
     render_by_row: str
@@ -18,21 +19,21 @@ docs = CLIDocs(
         "Specifies a column by name for partitioning. "
         "Partitions are defined by a column and the row(s) the partition shall contain. "
         "--column is mandatory if --rows is given and vice versa."
-               ),
+    ),
 
     rows=(
         "Specifies a row by name for partitioning. "
         "Partitions are defined by a column and the row(s) the partition shall contain. "
         "--rows is mandatory if --column is given and vice versa."
-               ),
+    ),
 
     render_by_row=(
         "Boolean flag. If active, the render_by_row strategy is applied for rendering. "
         "For rendering strategies see https://github.com/lu-pl/tabular#template-converters."
-               ),
+    ),
 
     format=(
         "Specifies a format for RDF serialization.\n"
         "This is a proxy for rdflib.Graph serialize."
-               )
+    )
 )
