d5644b6123b496a944b02830f3ed2bbb06c4d955
Lukas Plank <lupl@tuta.io>
2023-08-01 12:40:39 +0200

    Implemented more tests

--- tests/test_cli/test_dataframe_utils.py
+++ tests/test_cli/test_dataframe_utils.py
@@ -11,13 +11,14 @@ import pandas as pd
 from tabular.cli.dataframe_utils import (
     EXTENSION_READ_METHODS,
     UnknownExtensionError,
+    get_dataframe_from_file,
+    partition_dataframe,
     _get_dataframe_try_hard,
     _get_pandas_read_methods,
     _get_read_method_by_extension,
     )
 
 from tests.data import tables_path
-
 
 
 def test_get_read_method_by_extension():
@@ -82,11 +83,41 @@ def test_get_dataframe_try_hard():
 
 
 def test_get_dataframe_from_file():
-    ...
+    """Test for get_dataframe_fm_file.
+
+    Get a dataframe from both a file with a registered extension
+    and an unknown extension (holding the same data).
+    """
+    test_csv_path = tables_path / "test.csv"
+    test_txt_path = tables_path / "test.txt"
+
+    dataframe_from_extension = get_dataframe_from_file(test_csv_path)
+    dataframe_from_trial = get_dataframe_from_file(test_txt_path)
+
+    # both dataframes should be non-empty
+    assert not dataframe_from_extension.empty
+    assert not dataframe_from_trial.empty
+
+    # the difference of the two dataframes should be empty
+    assert dataframe_from_extension.compare(dataframe_from_trial).empty
 
 
 def test_partition_dataframe():
-    ...
 
+    test_csv_path = tables_path / "test.csv"
+    dataframe = get_dataframe_from_file(test_csv_path)
 
-test_get_dataframe_try_hard()
+    partition_by_column_1 = partition_dataframe(
+        dataframe=dataframe,
+        column="column 1 name",
+        rows=("first row data 1", "second row data 1")
+        )
+
+    partition_by_column_2 = partition_dataframe(
+        dataframe=dataframe,
+        column="column 2 name",
+        rows=("first row data 2", "second row data 2")
+        )
+
+    # partitions should be equal, i.e. the difference should be empty
+    assert partition_by_column_1.compare(partition_by_column_2).empty
