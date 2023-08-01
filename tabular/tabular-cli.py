91df30f25fc36d3938c3a9ede8a06e4be4363c9b
Lukas Plank <lupl@tuta.io>
2023-08-01 12:38:43 +0200

    Implemented CLI subcommands

--- tabular/tabular-cli.py
+++ tabular/tabular-cli.py
@@ -6,18 +6,20 @@ from typing import Any, Callable
 
 import click
 
-from cli.click_custom import (
-    RequiredIf,
-    RequiredMultiOptions,
-    DefaultCommandGroup
-)
+from tabular import TemplateConverter, TemplateGraphConverter
 
-from tabular_types import (
+from tabular.tabular_types import (
     _ClickPath,
     _GraphFormatOptions,
     _GraphFormatOptionsChoice
     )
 
+from tabular.cli.click_custom import (
+    RequiredIf,
+    RequiredMultiOptions,
+    DefaultCommandGroup
+)
+from tabular.cli.converter_setup import initialize_converter
 
 _common_options = [
     click.argument("table",
@@ -45,7 +47,11 @@ def common_options(f: Callable) -> Callable:
 
 @click.group(cls=DefaultCommandGroup)
 def tabular_cli():
-    """Actual CLI Docs."""
+    """TabulaR CLI.
+
+    Command-line interface for converting tabular data
+    using Jinja2 templating.
+    """
     pass
 
 
@@ -60,14 +66,21 @@ def noparse(table: pathlib.Path,
             column: str,
             rows: tuple[Any, ...],
             render_by_row):
-    """..."""
-    # 1. get a dataframe
-    # 1.2. optional: partition a dataframe
+    """Generate Jinja2 renderings without any parsing."""
+    # get converter
+    converter = initialize_converter(
+        converter_type=TemplateConverter,
+        table=table,
+        template=template,
+        column=column,
+        rows=rows
+        )
 
-    # 2. get a TemplateConverter
-
-    # 3. render according to strategy (table or row)
-    click.echo((table, template, column, rows, render_by_row))
+    # render according to strategy (table or row)
+    if render_by_row:
+        click.echo(converter.render_by_row())
+    else:
+        click.echo(converter.render())
 
 
 @tabular_cli.command()
@@ -80,14 +93,18 @@ def graph(table: pathlib.Path,
           column: str,
           rows: tuple[Any, ...],
           format: _GraphFormatOptions):
-    """..."""
-    # 1. get a dataframe
-    # 1.2. optional: partition a dataframe
+    """Generate and parse Jinja2 renderings into an rdflib.Graph."""
+    # get converter
+    converter = initialize_converter(
+        converter_type=TemplateGraphConverter,
+        table=table,
+        template=template,
+        column=column,
+        rows=rows
+        )
 
-    # 2. get a Converter
-
-    # 3. serialize from rdflib.Graph instance according to format
-    click.echo((table, template, column, rows, format))
+    # serialize from rdflib.Graph instance according to format
+    click.echo(converter.serialize(format=format))
 
 
 if __name__ == "__main__":
