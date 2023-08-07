7db69012524ba43045fe958b7aa246b7cb195f5f
Lukas Plank <lupl@tuta.io>
2023-08-07 19:01:35 +0200

    Added --context-module logic

--- tabular/cli/converter_setup.py
+++ tabular/cli/converter_setup.py
@@ -9,6 +9,7 @@ from tabular.cli.dataframe_utils import (
     get_dataframe_from_file,
     partition_dataframe
 )
+from tabular.cli.context_module import load_module, get_namespace_mapping
 
 
 # add data parameter for TemplateConverter!!! -> --context-module flag
@@ -16,7 +17,8 @@ def initialize_converter(converter_type: type[TemplateConverter],
                          table: pathlib.Path,
                          template: pathlib.Path,
                          column: Optional[str] = None,
-                         rows: Optional[tuple[Any, ...]] = None
+                         rows: Optional[tuple[Any, ...]] = None,
+                         context_module: pathlib.Path | None = None,
                          ) -> TemplateConverter:
     """Initialize a TemplateConverter.
 
@@ -26,7 +28,7 @@ def initialize_converter(converter_type: type[TemplateConverter],
     # 1. get a dataframe
     dataframe = get_dataframe_from_file(table)
 
-    # 1.2. optional: partition a dataframe
+    # 1.1 optional: partition a dataframe
     if column:  # column and rows are mutually dependent in the CLI
         dataframe = partition_dataframe(
             dataframe=dataframe,
@@ -34,10 +36,23 @@ def initialize_converter(converter_type: type[TemplateConverter],
             rows=rows
         )
 
+    # 1.2 optional: provide a module context
+    if context_module:
+        module_name = context_module.stem
+        # load the module
+        module = load_module(context_module, module_name)
+        # get a module dict
+        namespace_mapping = get_namespace_mapping(module)
+
+        data = {module_name: namespace_mapping}
+    else:
+        data = None
+
     # 2. get a TemplateConverter
     converter = converter_type(
         dataframe=dataframe,
-        template=template
+        template=template,
+        data=data
     )
 
     return converter
